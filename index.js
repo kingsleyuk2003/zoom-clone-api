3077f2a1ee095471782969f3bb0eb3d8010c004f
kingsleyuk2003 <kingsleyuk2003@yahoo.com>
2021-10-07 22:11:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const users = [];
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const addUser = (userName, roomId) => {
+  users.push({
+    userName: userName,
+    roomId: roomId,
+  });
+};
+
+const userLeave = (userName) => {
+  users = users.filter((user) => user.userName != userName);
+};
+
+const getRoomUsers = (roomId) => {
+  return users.filter((user) => user.roomId == roomId);
+};
+
+io.on("connection", (socket) => {
+  console.log("Someone Connected");
+  socket.on("join-room", (roomId, userName) => {
+    console.log("User Joined Room");
+    console.log(roomId, userName);
+    if (roomId && userName) {
+      socket.join(roomId);
+      addUser(userName, roomId);
+      socket.to(roomId).emit("user-connected", userName);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    }
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+      socket.leave(roomId);
+      userLeave(userName);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log("Zoom Clone API listening on localhost:3001");
+});
